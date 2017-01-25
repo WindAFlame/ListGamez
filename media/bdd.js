d78ca8d8f1365c0c5f1fa4a081c5a47cd2332da7
WindAFlame <paulolau35@hotmail.com>
2017-01-25 13:29:49 +0100

    Test web project with github

---
+++ media/bdd.js
@@ -0,0 +1,21 @@
+var bdd = [
+    {
+    "title": "Counter-Strike",
+    "picture": "../../media/add.jpg",
+    "cover": "../../media/hdnVg.jpg",
+    "system": [
+        "windows",
+        "linux"
+    ],
+    "link": "#"
+    },
+    {
+    "title": "HyperDimension Neptunia Re:Birth 3 V Generation",
+    "picture": "../../media/add.jpg",
+    "cover": "../../media/add.jpg",
+    "system": [
+        "windows"
+    ],
+    "link": "#"
+    }
+]
