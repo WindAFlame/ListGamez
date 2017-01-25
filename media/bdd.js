3fbf54692c8e2ba06430c3892ed7b134903525ea
WindAFlame <paulolau35@hotmail.com>
2017-01-25 13:35:03 +0100

    update index #2

--- media/bdd.js
+++ media/bdd.js
@@ -1,8 +1,8 @@
 var bdd = [
     {
     "title": "Counter-Strike",
-    "picture": "../../media/add.jpg",
-    "cover": "../../media/hdnVg.jpg",
+    "picture": "media/add.jpg",
+    "cover": "media/hdnVg.jpg",
     "system": [
         "windows",
         "linux"
@@ -11,8 +11,8 @@ var bdd = [
     },
     {
     "title": "HyperDimension Neptunia Re:Birth 3 V Generation",
-    "picture": "../../media/add.jpg",
-    "cover": "../../media/add.jpg",
+    "picture": "media/add.jpg",
+    "cover": "media/add.jpg",
     "system": [
         "windows"
     ],
