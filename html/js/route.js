9f5ae6e5d2789cb40f9208d7735c0086e7577e07
WindAFlame <paulolau35@hotmail.com>
2017-01-26 14:44:04 +0100

    change footer and route url

--- html/js/route.js
+++ html/js/route.js
@@ -1,27 +1,27 @@
 app.config(function($stateProvider, $urlRouterProvider) {
     
-    $urlRouterProvider.otherwise('/');
+    $urlRouterProvider.otherwise('#');
     
     $stateProvider
         
         // HOME PAGE - Welcome
         .state('home', {
             name: 'home',
-            url: '/',
+            url: '#',
             templateUrl: 'html/view/homepage.htm'
         })
 
         // LIST GAME - Listing of all game from json
         .state('list', {
             name: 'list',
-            url: '/list',
+            url: '#list',
             templateUrl: 'html/view/list.htm'
         })
         
         // DETAIL GAME - Detail for a unique game
         .state('detail', {
             name: 'detail',
-            url: '/detail',
+            url: '#detail',
             template: '<h1>Not Implemented</h1>'
         });
 
