2f11a0648a3c331ad014b3bcb67a5c0ade6b4e1e
WindAFlame <paulolau35@hotmail.com>
2017-03-08 16:42:21 +0100

    add route

--- html/js/route.js
+++ html/js/route.js
@@ -22,6 +22,23 @@ app.config(function($stateProvider, $urlRouterProvider) {
         .state('detail', {
             name: 'detail',
             url: '/detail',
+            params: {
+                id : null
+            },
+            template: '<h1>{id}</h1>'
+        })
+        
+        // ABOUT - Detail of me and project
+        .state('about', {
+            name: 'about',
+            url: '/about',
+            template: '<h1>Not Implemented</h1>'
+        })
+        
+        // CONTACT - Contact me for any problem with this website
+        .state('contact', {
+            name: 'contact',
+            url: '/contact',
             template: '<h1>Not Implemented</h1>'
         });
 
