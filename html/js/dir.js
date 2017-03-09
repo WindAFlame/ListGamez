b6e90ae8f1eb6426e35468b2e908c159a859f466
WindAFlame <paulolau35@hotmail.com>
2017-03-09 15:49:12 +0100

    try try try try try fix

--- html/js/dir.js
+++ html/js/dir.js
@@ -1,10 +1,10 @@
-app.directive('datafromlocaljson', ['$http', '$scope', function($http, $scope) {
+app.directive('datafromlocaljson', ['$http', function($http) {
     return {
             restrict: 'E',
             scope:{
                 src:"="
             },
-            link: function($rootScope) {
+            link: function($scope, $rootScope) {
                 $http.get($scope.src)
                     .then(function(res){
                         $rootScope.data = res.data;
