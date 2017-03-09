53f87bd46e0a9d2e8898a6cf58856164788cdfd6
WindAFlame <paulolau35@hotmail.com>
2017-03-09 10:15:26 +0100

    Add log on id

--- html/js/ctrl.js
+++ html/js/ctrl.js
@@ -10,6 +10,7 @@ app.controller('listCtrl', function($scope,$http){
 app.controller('detailCtrl', function($scope,$http,$stateParams){
     // Test with $stateParams
     console.log('StateParams = '+$stateParams+'.');
+    console.log('StateParams.id = '+$stateParams.id+'.');
     // Load Bdd
     $http.get('media/bdd.json')
        .then(function(res){
