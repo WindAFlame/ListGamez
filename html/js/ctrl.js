e4b0afe71f178bc2945f60e798aae6d750038148
WindAFlame <paulolau35@hotmail.com>
2017-03-09 10:33:31 +0100

    same previous

--- html/js/ctrl.js
+++ html/js/ctrl.js
@@ -10,7 +10,7 @@ app.controller('listCtrl', function($scope,$http){
 app.controller('detailCtrl', function($scope,$http,$stateParams){
     // Test with $stateParams
     console.log('StateParams = '+$stateParams+'.');
-    console.log('StateParams.id = '+$stateParams.id+'.');
+    console.log('StateParams.gameid = '+$stateParams.gameId+'.');
     // Load Bdd
     $http.get('media/bdd.json')
        .then(function(res){
