a03c2747118eb830e34e7cd0999a572b9b95cb23
WindAFlame <paulolau35@hotmail.com>
2017-03-09 15:33:10 +0100

    try use

--- html/js/ctrl.js
+++ html/js/ctrl.js
@@ -1,3 +1,9 @@
+app.controller('listCtrl', function($scope,$http){
+    console.log("listCtrl ="+$scope.data);
+    $scope.games = $scope.data;
+});
+
+
 app.controller('detailCtrl', function($scope,$http,$stateParams,$filter){
   $scope.gameById = $filter('filter')($scope.data, {id: $stateParams.gameId });
 });
