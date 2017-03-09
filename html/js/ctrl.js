583611fa19fc90fadce57cd2ea64b43c4d5d105c
WindAFlame <paulolau35@hotmail.com>
2017-03-09 15:59:26 +0100

    Revert to 56e05fced

--- html/js/ctrl.js
+++ html/js/ctrl.js
@@ -1,3 +1,17 @@
-app.controller('detailCtrl', function($scope,$rootScope,$stateParams,$filter){
-  $scope.gameById = $filter('filter')($rootScope.data, {id: $stateParams.gameId });
+app.controller('listCtrl', function($scope,$http){
+    // Load Bdd
+    $http.get('media/bdd.json')
+       .then(function(res){
+          $scope.games = res.data;                
+        });
+});
+
+
+app.controller('detailCtrl', function($scope,$http,$stateParams,$filter){
+    
+    // Load Bdd
+    $http.get('media/bdd.json')
+       .then(function(res){
+          $scope.gameById = $filter('filter')(res.data, {id: $stateParams.gameId });
+        });
 });
