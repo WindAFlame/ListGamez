12d3b250381819f0195139ba16bc998b2648ebf3
WindAFlame <paulolau35@hotmail.com>
2017-03-09 15:11:46 +0100

    add dir to load bdd

--- html/js/ctrl.js
+++ html/js/ctrl.js
@@ -1,17 +1,3 @@
-app.controller('listCtrl', function($scope,$http){
-    // Load Bdd
-    $http.get('media/bdd.json')
-       .then(function(res){
-          $scope.games = res.data;                
-        });
-});
-
-
 app.controller('detailCtrl', function($scope,$http,$stateParams,$filter){
-    
-    // Load Bdd
-    $http.get('media/bdd.json')
-       .then(function(res){
-          $scope.gameById = $filter('filter')(res.data, {id: $stateParams.gameId });
-        });
+  $scope.gameById = $filter('filter')($scope.data, {id: $stateParams.gameId });
 });
---
+++ html/js/dir.js
@@ -0,0 +1,14 @@
+app.directive('datafromjson', function(){
+    return {
+            restrict: 'A',
+            link: function($scope) {
+                $http.get('media/bdd.json')
+                    .then(function(res){
+                        $scope.data = res.data;
+                        if ($scope.data!=null){        
+                            console.log("Data loaded from json.")
+                        }
+                    });
+            }
+        };
+});
