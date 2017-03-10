ee45e60d0f7ce5fd2fb4efeb2c808bd491082d7f
WindAFlame <paulolau35@hotmail.com>
2017-03-10 11:04:02 +0100

    test child with uiview

--- html/js/route.js
+++ html/js/route.js
@@ -4,38 +4,44 @@ app.config(function($stateProvider, $urlRouterProvider) {
 
     $stateProvider
         
+        // Template for all view
+        .state('base', {
+            abstract: true,
+            templateUrl: "index.html"
+        })
+
         // HOME PAGE - Welcome
         .state('home', {
             name: 'home',
-            url: '/',
+            url: '/index.html',
             templateUrl: 'html/view/homepage.htm'
         })
 
         // LIST GAME - Listing of all game from json
         .state('list', {
             name: 'list',
-            url: '/list',
+            url: '/list.html',
             templateUrl: 'html/view/list.htm'
         })
         
         // DETAIL GAME - Detail for a unique game
         .state('detail', {
             name: 'detail',
-            url: '/detail/{gameId}',
+            url: '/detail.html/{gameId}',
             templateUrl: 'html/view/detail.htm'
         })
         
         // ABOUT - Detail of me and project
         .state('about', {
             name: 'about',
-            url: '/about',
+            url: '/about.html',
             template: '<h1>Not Implemented</h1>'
         })
         
         // CONTACT - Contact me for any problem with this website
         .state('contact', {
             name: 'contact',
-            url: '/contact',
+            url: '/contact.html',
             template: '<h1>Not Implemented</h1>'
         });
 
