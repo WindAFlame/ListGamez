431fa959aadb10e7f3439638d4bbefc7d0a03c4d
WindAFlame <paulolau35@hotmail.com>
2017-03-10 11:11:52 +0100

    fix state

--- html/js/route.js
+++ html/js/route.js
@@ -3,43 +3,37 @@ app.config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/');
 
     $stateProvider
-        
-        // Template for all view
-        .state('base', {
-            abstract: true,
-            templateUrl: 'index.html'
-        })
 
         // HOME PAGE - Welcome
-        .state('base.home', {
+        .state('home', {
             name: 'home',
             url: '/index.html',
             templateUrl: 'html/view/homepage.htm'
         })
 
         // LIST GAME - Listing of all game from json
-        .state('base.list', {
+        .state('list', {
             name: 'list',
             url: '/list.html',
             templateUrl: 'html/view/list.htm'
         })
         
         // DETAIL GAME - Detail for a unique game
-        .state('base.detail', {
+        .state('detail', {
             name: 'detail',
             url: '/{gameId}',
             templateUrl: 'html/view/detail.htm'
         })
         
         // ABOUT - Detail of me and project
-        .state('base.about', {
+        .state('about', {
             name: 'about',
             url: '/about.html',
             template: '<h1>Not Implemented</h1>'
         })
         
         // CONTACT - Contact me for any problem with this website
-        .state('base.contact', {
+        .state('contact', {
             name: 'contact',
             url: '/contact.html',
             template: '<h1>Not Implemented</h1>'
