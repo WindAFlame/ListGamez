5238c5ec3fcabc0a31e36f787c6160a9eef026e6
WindAFlame <paulolau35@hotmail.com>
2017-03-10 10:06:35 +0100

    delete old function

--- html/js/dialog.js
+++ html/js/dialog.js
@@ -15,16 +15,3 @@
         showButton.addEventListener('click', showClickHandler);
         closeButton.addEventListener('click', closeClickHandler);
     }());
-
-
-var dialog = document.querySelector('dialog');
-var showDialogButton = document.querySelector('#show-dialog');
-if (! dialog.showModal) {
-    dialogPolyfill.registerDialog(dialog);
-}
-showDialogButton.addEventListener('click', function() {
-    dialog.showModal();
-});
-dialog.querySelector('.close').addEventListener('click', function() {
-    dialog.close();
-});
