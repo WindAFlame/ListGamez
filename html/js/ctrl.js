5fee7a29130bce3366ff6fe1e8d5132c3cd7f772
WindAFlame <paulolau35@hotmail.com>
2017-03-17 12:01:05 +0100

    try fix animate

--- html/js/ctrl.js
+++ html/js/ctrl.js
@@ -4,4 +4,5 @@ app.controller('gameCtrl', function($scope, gamesData){
 
 app.controller('gameDetailCtrl', function($scope,$stateParams,$filter){
     $scope.gameById = $filter('filter')($scope.games, {id: $stateParams.gameId })[0];
+    componentHandler.upgradeDom();
 });
