34c7a02aad17a1c989b326d388bf0b39cf04dcbe
WindAflame <dev.windaflame@gmail.com>
2019-07-13 23:24:42 +0200

    Replace the library fontawesome with the official.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEdit as farEdit } from '@fortawesome/free-regular-svg-icons';
+import { faDatabase, faDownload, faExternalLinkAlt, faFileImport, faFileUpload, faMagnet } from '@fortawesome/free-solid-svg-icons';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-rooting.module';
-import { AngularFontAwesomeModule } from 'angular-font-awesome';
-
 import { ArticleDetailComponent } from './article/detail/detail.component';
 import { ArticleListComponent } from './article/list/list.component';
 import { HomeComponent } from './home/home.component';
-import { TemplateComponent } from './template/template.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { TemplateSearchBarComponent } from './template/search-bar/search-bar.component';
+import { TemplateComponent } from './template/template.component';
 
 
 @NgModule({
@@ -27,9 +29,14 @@ import { TemplateSearchBarComponent } from './template/search-bar/search-bar.com
     ReactiveFormsModule,
     NgbModule,
     AppRoutingModule,
-    AngularFontAwesomeModule
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [TemplateComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    // Add an icon to the library for convenient access in other components
+    library.add(farEdit, faDownload, faFileImport, faFileUpload, faDatabase, faMagnet, faExternalLinkAlt);
+  }
+}
